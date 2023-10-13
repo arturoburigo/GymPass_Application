40e26f957895447bf47db76f85d6068b4843f0c1
Arturo Burigo <arturoburigo@Arturos-MacBook-Pro.local>
2023-10-13 00:18:00 -0300

    changes

---
+++ src/http/controllers/register-controller.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod'
+import { prisma } from '../../lib/prisma'
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function createUser(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    email: z.string().email(),
+    name: z.string(),
+    password: z.string().min(6),
+  })
+
+  const { email, name, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+}
